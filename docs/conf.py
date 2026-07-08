b24e3f28dbb2e9882ed8b1d1b7b266e57d923c23
pcmaticmalwarenotdetected <thunderbridangel@outlook.com>
2026-07-08 17:28:39 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+project = 'spypoint-login'
+author = 'spypoint-login'
+release = '1.0'
+
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Theme
+html_theme = 'alabaster'
+html_static_path = ['_static']
